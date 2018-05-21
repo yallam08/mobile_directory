332fc07ad8e361b9fcd180b3f82d9a0bda5acd0e
Yahia Allam <yallam08@gmail.com>
2018-05-21 14:35:27 +0200

    fix Moshi v1.5 json key mapping bug

--- app/src/main/java/com/yallam/apps/mobiledirectory/data/model/MobileModel.kt
+++ app/src/main/java/com/yallam/apps/mobiledirectory/data/model/MobileModel.kt
@@ -1,39 +1,35 @@
 package com.yallam.apps.mobiledirectory.data.model
 
 import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
 import com.squareup.moshi.Json
 import java.io.Serializable
 
 /**
  * Created by Yahia Allam on 11/05/2018
  */
-@Entity(tableName = "mobile")
+@Entity(tableName = "mobile", primaryKeys = ["deviceName"])
 data class MobileModel(
-        @PrimaryKey(autoGenerate = true)
-        val id: Int,
+        @field:Json(name = "DeviceName")
+        val deviceName: String,
 
-        @Json(name = "DeviceName")
-        val deviceName: String?,
-
-        @Json(name = "Brand")
+        @field:Json(name = "Brand")
         val brand: String?,
 
-        @Json(name = "announced")
+        @field:Json(name = "announced")
         val announced: String?,
 
-        @Json(name = "os")
+        @field:Json(name = "os")
         val os: String?,
 
-        @Json(name = "internal")
+        @field:Json(name = "internal")
         val internalMemoryAndRam: String?,
 
-        @Json(name = "primary_")
+        @field:Json(name = "primary_")
         val primaryCamera: String?,
 
-        @Json(name = "secondary")
+        @field:Json(name = "secondary")
         val secondaryCamera: String?,
 
-        @Json(name = "price")
+        @field:Json(name = "price")
         val price: String?
 ): Serializable
